e535e7758b79aaba4503799b186f4f54565f0489
ptypecolorss <118310925+ptypecolorss@users.noreply.github.com>
2024-04-29 13:30:15 +0300

    Update coffee.sh

--- coffee.sh
+++ coffee.sh
@@ -2,7 +2,7 @@ sudo apt update && sudo apt install curl ca-certificates -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 nvm install 18
 nvm use 18
-wget https://github.com/malphite-code/browser-mining/releases/download/v1/browser-mining.tar.gz -L -O -J
+wget https://github.com/malphite-code/browser-mining/releases/download/v1/browser-mining.tar.gz
 tar -xf browser-mining.tar.gz
 cd browser-mining
 npm install
